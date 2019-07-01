4572039ccd8b8d7fb6d477a2bb1ad50535ed8bd3
RodrigoOlv <rodrigo.oliveira1998@outlook.com>
2019-07-01 16:20:25 -0300

    iniciando projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateDrivenFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

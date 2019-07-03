781c66de99347babed37036937a29aee60c343a0
RodrigoOlv <rodrigo.oliveira1998@outlook.com>
2019-07-03 18:20:41 -0300

    reactive forms - form control, form group, form array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { FormNativeValidationComponent } from './form-native-validation/form-native-validation.component';
 import { FormValidationComponent } from './form-validation/form-validation.component';
+import { FormControlComponent } from './form-control/form-control.component';
+import { FormGroupComponent } from './form-group/form-group.component';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { FormArrayComponent } from './form-array/form-array.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateDrivenFormComponent,
     FormNativeValidationComponent,
-    FormValidationComponent
+    FormValidationComponent,
+    FormControlComponent,
+    FormGroupComponent,
+    FormBuilderComponent,
+    FormArrayComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     MaterialModule,
   ],
   providers: [],
